2b6105953e31ab04a28bede8c4fb4d6b1f2159e4
qwcan <ieatspamforbreakfasts1@gmail.com>
2026-02-04 00:12:17 -0600

    Add barber patch too

---
+++ BarberVisibilityTweaks/Plugin.cs
@@ -0,0 +1,66 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using qwcanBarber.patch;
+using BepInEx.Configuration;
+using LethalConfig;
+using LethalConfig.ConfigItems;
+using LethalConfig.ConfigItems.Options;
+
+
+namespace qwcanBarber;
+
+
+[BepInPlugin("BarberVisibilityTweaks", "BarberVisibilityTweaks", "1.0.0")]
+[BepInDependency("ainavt.lc.lethalconfig")]
+public class Plugin : BaseUnityPlugin
+{
+    public static Plugin Instance { get; set; }
+
+    public static ManualLogSource Log => Instance.Logger;
+
+    private readonly Harmony _harmony = new("BarberVisibilityTweaks");
+
+
+    public ConfigEntry<float> minFadingDistance;
+    public ConfigEntry<float> maxFadingDistance;
+    
+    public Plugin()
+    {
+        Instance = this;
+    }
+
+    private void Awake()
+    {
+        minFadingDistance = Config.Bind("Fading", "Minimum Fading Distance", 5f, "Distance at which the barber will be fully opaque");
+        maxFadingDistance = Config.Bind("Fading", "Maximum Fading Distance", 7f, "Distance at which the barber will start fading in");
+        var minInput = new FloatStepSliderConfigItem(minFadingDistance, new FloatStepSliderOptions()
+        {
+            Min = 0,
+            Max = 100,
+            Step = 0.5f,
+            RequiresRestart = false
+        });
+        LethalConfigManager.AddConfigItem(minInput);
+        var maxInput = new FloatStepSliderConfigItem(maxFadingDistance, new FloatStepSliderOptions()
+        {
+            Min = 0,
+            Max = 100,
+            Step = 0.5f,
+            RequiresRestart = false
+        });
+        LethalConfigManager.AddConfigItem(maxInput);
+        
+        Log.LogInfo($"Applying patches...");
+        ApplyPluginPatch();
+        Log.LogInfo($"Patches applied");
+    }
+
+    /// <summary>
+    /// Applies the patch to the game.
+    /// </summary>
+    private void ApplyPluginPatch()
+    {
+        _harmony.PatchAll(typeof(BarberPatch));
+    }
+}
---
+++ BarberVisibilityTweaks/patch/BarberPatch.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using BepInEx.Logging;
+using HarmonyLib;
+using qwcanBarber;
+using UnityEngine;
+using LethalConfig;
+
+namespace qwcanBarber.patch;
+
+/// <summary>
+/// Patch to modify the behavior of the claysurgeon/barber. It won't kill players unless it's moving, and will optionally do damage instead.
+/// </summary>
+[HarmonyPatch(typeof(ClaySurgeonAI))]
+public class BarberPatch
+{
+
+    /*
+    [HarmonyPatch(nameof(ClaySurgeonAI.OnCollideWithPlayer))]
+    [HarmonyPrefix]
+    private static bool OnCollideWithPlayer(ref ClaySurgeonAI __instance, ref Collider other)
+    {
+        Debug.Log($"Min: {__instance.minDistance}");
+        Debug.Log($"Max: {__instance.maxDistance}");
+        if (!__instance.isJumping)
+        {
+            //Debug.Log( "Not jumping, not killing player");
+            return false;
+        }
+
+        return true;
+    }
+    */
+    
+    [HarmonyPatch(typeof(ClaySurgeonAI), "SetVisibility")]
+    [HarmonyPrefix]
+    private static void SetVisibility(ref ClaySurgeonAI __instance)
+    {
+        Debug.Log($"Min: {__instance.minDistance}");
+        Debug.Log($"Max: {__instance.maxDistance}");
+        __instance.minDistance = Plugin.Instance.minFadingDistance.Value;
+        __instance.maxDistance = Plugin.Instance.maxFadingDistance.Value;
+
+    }
+}
